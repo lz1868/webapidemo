9de77ef28d4bdc3cdb68d5dca2ddef38637a23cd
Aaron Zhang <aaron.cheung@hotmail.com>
2013-09-05 17:52:33 +0800

    Initial version

---
+++ src/WebAPIDemo/WebAPIDemo/Models/IContactRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebAPIDemo.Models
+{
+    public interface IContactRepository { 
+        IEnumerable<Contact> GetAllContacts(); 
+        Contact GetContact(string id); 
+        Contact AddContact(Contact item); 
+        bool RemoveContact(string id); 
+        bool UpdateContact(string id, Contact item);   
+    }
+}
